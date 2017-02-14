1a3dfd44f9d92586726cfa5913a5d4b30305a3c5
Danilo Horta <danilo.horta@gmail.com>
2017-02-14 16:27:58 +0000

    automatic

--- travis/install_blosc.sh
+++ travis/install_blosc.sh
@@ -1,14 +1,5 @@
 #/bin/sh -f
 
-if ( test "`uname -s`" = "Darwin" )
-then
-  echo
-else
-  sudo add-apt-repository --yes ppa:kalakris/cmake
-  sudo apt-get update -qq
-  sudo apt-get install cmake
-fi
-
 wget https://github.com/Blosc/c-blosc/archive/v1.11.2.tar.gz
 tar xzf v1.11.2.tar.gz
 

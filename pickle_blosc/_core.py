f227e2da7fd05b63c1057303ed7261967c673986
Danilo Horta <danilo.horta@gmail.com>
2019-05-31 18:40:29 +0100

    new

--- pickle_blosc/_core.py
+++ pickle_blosc/_core.py
@@ -1,17 +1,21 @@
-from __future__ import absolute_import
+import pickle as pkl
 
 import blosc
 
-try:
-    import cPickle as pkl
-except ImportError:
-    import pickle as pkl
-
 
 def pickle(obj, filepath):
-    """Pickle and compress."""
+    """
+    Pickle, compress, and save it to a file.
+
+    Parameters
+    ----------
+    obj : object
+        Any python object to be pickled.
+    filepath : str
+        File path destination.
+    """
     arr = pkl.dumps(obj, -1)
-    with open(filepath, 'wb') as f:
+    with open(filepath, "wb") as f:
         s = 0
         while s < len(arr):
             e = min(s + blosc.MAX_BUFFERSIZE, len(arr))
@@ -21,9 +25,21 @@ def pickle(obj, filepath):
 
 
 def unpickle(filepath):
-    """Decompress and unpickle."""
+    """
+    Read, decompress, and unpickle a python object.
+
+    Parameters
+    ----------
+    filepath : str
+        File path to a pickled file.
+
+    Returns
+    -------
+    obj : object
+        Unpickled object.
+    """
     arr = []
-    with open(filepath, 'rb') as f:
+    with open(filepath, "rb") as f:
         carr = f.read(blosc.MAX_BUFFERSIZE)
         while len(carr) > 0:
             arr.append(blosc.decompress(carr))

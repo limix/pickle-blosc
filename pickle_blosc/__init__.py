445578bf1eb1ff67c846560b93957357201158fb
Danilo Horta <danilo.horta@gmail.com>
2019-05-31 20:47:28 +0100

    0.1.1

--- pickle_blosc/__init__.py
+++ pickle_blosc/__init__.py
@@ -5,7 +5,7 @@ Library for reading and writting Pickle files using Blosc compression.
 from ._core import pickle, unpickle
 from ._testit import test
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 __all__ = ["__version__", "test", "pickle", "unpickle"]
